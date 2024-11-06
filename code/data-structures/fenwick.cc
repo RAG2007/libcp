e19c19d68c3edcac29c4c3c18165a23e313a6e8e
RAG2007 <sto.galinskir@gmail.com>
2024-11-06 19:39:07 +0100

    fixed fenwick

--- code/data-structures/fenwick.cc
+++ code/data-structures/fenwick.cc
@@ -1,20 +1,16 @@
-#include <bits/stdc++.h>
-
-using namespace std;
-
 struct fenwick {
         vector<long long> values;
         fenwick(int size) {
                 values.resize(size);
         }
         void update(int index, long long delta) {
-                for (;index < (int)values.size(); index |= (index + 1))
+                for (; index < (int)values.size(); index |= (index + 1))
                         values[index] += delta;
         }
         long long query(int index) {
                 long long sum = 0;
-                for (;index >= 0; index &= index + 1)
-                        sum += values[index--];
+                for (index++; index > 0; index &= (index - 1))
+                        sum += values[index - 1];
                 return sum;
         }
         long long query(int l, int r){
