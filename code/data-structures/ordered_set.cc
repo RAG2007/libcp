d4288f481c619982120920a4e15c50ca2196014d
RAG2007 <76128078+RAG2007@users.noreply.github.com>
2024-11-26 18:01:06 +0100

    added ordered set

---
+++ code/data-structures/ordered_set.cc
@@ -0,0 +1,19 @@
+/*
+* wklejone z github.com/tonowak/acmlib
+* Opis:
+*   \texttt{insert(x)} dodaje element $x$ (nie ma emplace),
+*   \texttt{find\_by\_order(i)} zwraca iterator do $i$-tego elementu,
+*   \texttt{order\_of\_key(x)} zwraca ile jest mniejszych elementów
+*   ($x$ nie musi być w secie).
+*   Jeśli chcemy multiseta, to używamy par $(val, id)$.
+*/
+#include<ext/pb_ds/assoc_container.hpp>
+#include<ext/pb_ds/tree_policy.hpp>
+using namespace __gnu_pbds;
+template<class T> using ordered_set = tree<
+	T,
+	null_type,
+	less<T>,
+	rb_tree_tag,
+	tree_order_statistics_node_update
+>;
